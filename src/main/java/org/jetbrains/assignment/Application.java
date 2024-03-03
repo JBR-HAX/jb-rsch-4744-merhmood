8f5f3211e0ca7ff8406c607241c45cbf7b2ef9f3
merhmood <mahmudsuberu9@gmail.com>
2024-03-03 15:16:30 +0100

    jetbrains survey task

--- src/main/java/org/jetbrains/assignment/Application.java
+++ src/main/java/org/jetbrains/assignment/Application.java
@@ -3,11 +3,44 @@ package org.jetbrains.assignment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 
+}
+
+@RestController
+class ApplicationRestController {
+    ArrayList<HashMap> locationsResult = new ArrayList<HashMap>();
+    Map<String, Integer> currentLocation = new HashMap<String, Integer>();
+    currentLocation.put("x", 0);
+    currentLocation.put("y", 0);
+
+    @PostMapping("/locations")
+    public String (@RequestBody String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'U') {
+                currentLocation.put("y", currentLocation.get("y") + 1);
+            } else if (input.charAt(i) == 'D') {
+                currentLocation.put("y", currentLocation.get("y") - 1);
+            } else if (input.charAt(i) == 'L') {
+                currentLocation.put("x", currentLocation.get("x") - 1);
+            } else if (input.charAt(i) == 'R') {
+                currentLocation.put("x", currentLocation.get("x") + 1);
+            }
+            locationsResult.add(currentLocation);
+        }
+    }
+    @PostMapping("/moves")
+    public String (@RequestBody String input) {
+        return "Hello World!";
+    }
 }
